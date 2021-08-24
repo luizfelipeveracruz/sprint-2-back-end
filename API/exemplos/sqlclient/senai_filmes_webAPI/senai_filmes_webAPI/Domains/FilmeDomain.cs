a3777559f664e55a55e57441b31bda379b0d31cd
luizfelipeveracruz <luizfelipeveracruz2010@gmail.com>
2021-08-24 10:58:37 -0300

    exemplo de criação de um projeto API e classe

---
+++ API/exemplos/sqlclient/senai_filmes_webAPI/senai_filmes_webAPI/Domains/FilmeDomain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_filmes_webAPI.Domains
+{
+    /// <summary>
+    /// Classe que representa a entidade (tabela) FILMES
+    /// </summary>
+    public class FilmeDomain
+    {
+        public int idFilme { get; set; }
+        public int idGenero { get; set; }
+        public string tituloFilme { get; set; }
+
+
+    }
+}
